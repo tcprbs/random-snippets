45370604a6bc406cd8f11877f1245a34f39e0f55
tcprbs <rbsdevops+tcp@gmail.com>
2019-01-21 22:30:26 +0800

    Decompress a string

---
+++ decompress.py
@@ -0,0 +1,23 @@
+def decompress(s):
+    result = ""
+    temp = []
+    n_times = ""
+    i = 0
+
+    while i < len(s):
+        char = s[i]
+        if ord(char) >= 48 and ord(char) <= 57:
+            n_times += char
+        elif char == '[':
+            while s[i] != ']':
+                i += 1
+                if s[i] != ']':
+                    temp.append(s[i])
+            if temp:
+               repeated_str = "".join(temp)
+               result += (repeated_str * int(n_times))
+               n_times = ""
+               temp = []
+        else:
+            result += char
+        i += 1
